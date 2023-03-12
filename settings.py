622ef19ef2ea622882344c4ad7ef2b2ed1fff24d
LumiSmile <sto_mat@yahoo.com>
2023-03-12 13:52:02 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,26 @@
+class Settings:
+    chrome_driver = "/tests/chromedriver"
+
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'djtata@icloud.com'
+    valid_password = 'Olexia2022'
+    valid_email_reg = 'djtata@icloud.com'
+    invalid_email = 'invalid.mail.com'
+    invalid_password = 'invalidpassw777'
+    empty_email = ''
+    empty_password = ''
+    menu_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    first_name = 'Брайан'
+    last_name = 'Кинни'
+    first_name_ = '-'
+    last_name_ = '-'
+    russian_generate_string = 'г'
+    latin_generate_string = 'citygrass'
+    special_chars = '@#$%^&*()-+=`~/.?><|b/'
+    chinese_chars = '秋天來到了莫斯科'
+    numbers = 351
+    empty = ''
+    passw1 = 'QwerTy123'
+    passw2 = 'Jkfuvlkdvns'
+    passw3 = 'Kivmlk3557u0985mn8'
